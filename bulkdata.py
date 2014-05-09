220c7d39fb44fb46d977268971b7bc09fd6e2908
David Rutherford <david.rutherford@workshare.com>
2014-05-09 18:25:38 +0100

    evecentralapi-client
    now with npc system and region db file and modified to find biggest
    difference between  buy/sell orders and calculate the numbers of jumps.
    Next wire in item ID list as db table.

---
+++ bulkdata.py
@@ -0,0 +1,347 @@
+import requests as req
+from xml.etree import ElementTree as ET
+from HTMLParser import HTMLParser
+import sqlite3 as SQL
+
+def getstations(url):
+     rv = req.get(url)
+     return rv.text
+
+class MyHTMLParser(HTMLParser):
+     def __init__(self):
+          HTMLParser.__init__(self)
+          self.intable = False
+          self.value = {}
+          self.diction = []
+          self.righttable = False
+          self.a = False
+          self.h2 = False
+          self.regionname = False
+          self.region = ""
+          self.working = []
+          
+     def handle_starttag(self, tag, attrs):
+          if tag == 'table':
+               self.intable = True
+          elif tag == 'h2':
+               self.h2 = True
+          elif tag == 'a':
+               self.a = True
+               for x in attrs:
+                    #print x
+                    if '/region/' in x[1].encode('ascii', 'ignore'):
+                         self.regionname = True
+                                
+     def handle_endtag(self, tag):
+          if tag == 'table':
+               self.intable = False
+               if self.righttable == True:
+                    self.value[self.region] = self.working
+                    self.working = []
+                    self.righttable = False
+          elif tag  == 'tr' and self.righttable == True and self.diction != []:
+                              
+               self.working.append(self.diction)
+               self.diction = []
+          
+          elif tag == 'h2':
+               self.h2 = False
+               self.regionname = False
+          elif tag == 'a':
+               self.regionname = False
+               self.a == False
+               
+               
+     def handle_data(self, data):
+          if self.h2 == True and self.a == True and self.regionname == True:
+               self.region = data.encode('ascii', 'ignore')
+          if 'Constellation' in data and self.intable == True:
+               self.righttable = True
+          if self.righttable == True:
+               temp = data.encode('ascii', 'ignore')
+               if not '\n' in temp and temp != ' ':
+                    if (temp not in ('Constellation', 'System', 'Security', 'Name', 'Type')):
+                         self.diction.append(temp)
+     
+     def returnvalue(self):
+          return self.value
+                    
+     
+
+if __name__=="__main__":
+     systems = []
+     npc_stations= ['24th Imperial Crusade', 
+              'Amarr Certified News', 
+              'Amarr Civil Service', 
+              'Amarr Constructions', 
+              'Amarr Navy', 
+              'Amarr Templars', 
+              'Amarr Trade Registry', 
+              'Ametat Security', 
+              'Ardishapur Family', 
+              'Bragian Order', 
+              'Carthum Conglomerate', 
+              'Civic Court', 
+              'Company of Marcher Lords', 
+              'Court Chamberlain', 
+              'Ducia Foundry', 
+              'Emperor Family', 
+              'Fraternity of St. Venefice', 
+              'Further Foodstuffs', 
+              'Hedion University', 
+              'Holdfast Syndicate', 
+              'HZO Refinery', 
+              'Imperial Academy', 
+              'Imperial Armaments', 
+              'Imperial Chancellor', 
+              'Imperial Guard', 
+              'Imperial Shipment', 
+              'Inherent Implants', 
+              'Joint Harvesting', 
+              'Kador Family', 
+              'Kameira Lodge', 
+              'Kor-Azor Family', 
+              'Ministry of Assessment', 
+              'Ministry of Internal Order', 
+              'Ministry of War', 
+              'Noble Appliances', 
+              'Nurtura', 
+              'Paladin Survey Force', 
+              'Red and Silver Hand', 
+              'Royal Amarr Institute', 
+              'Royal Uhlans', 
+              'Sarum Family', 
+              'Shining Flame', 
+              'Tal-Romon Legion', 
+              'Tash-Murkon Family', 
+              'Theology Council', 
+              'Viziam', 
+              'Zoar and Sons', 'Ammatar Consulate',
+              'Ammatar Fleet',
+              'Nefantar Miner Association',
+              'Archangels',
+              'Dominations',
+              'Guardian Angels',
+              'Salvation Angels',
+              'Blood Raiders',
+              'Caldari Business Tribunal',
+              'Caldari Constructions',
+              'Caldari Funds Unlimited',
+              'Caldari Navy',
+              'Caldari Provisions',
+              'Caldari Steel',
+              'CBD Corporation',
+              'CBD Sell Division',
+              'Chief Executive Panel',
+              'Commando Perkone',
+              'Corporate Police Force',
+              'Deep Core Mining Inc.',
+              'Echelon Entertainment',
+              'Expert Distribution',
+              'Expert Housing',
+              'Expert Intervention',
+              'Home Guard',
+              'House of Records',
+              'Hyasyoda Corporation',
+              'Ikomari-Onu Enforcement',
+              'Intara Direct Action',
+              'Internal Security',
+              'Ishukone Corporation',
+              'Ishukone Watch',
+              'Isuuaya Tactical',
+              'Kaalakiota Corporation',
+              'Kinsho Swords',
+              'Kirkinen Risk Control',
+              'Lai Dai Corporation',
+              'Lai Dai Protection Service',
+              'Mercantile Club',
+              'Minedrill',
+              'Modern Finances',
+              'Nugoeihuvi Corporation',
+              'Onikanabo Brigade',
+              'Osmon Surveillance',
+              'Peace and Order Unit',
+              'Perkone',
+              'Poksu Mineral Group',
+              'Prompt Delivery',
+              'Propel Dynamics',
+              'Rapid Assembly',
+              'School of Applied Knowledge',
+              'Science and Trade Institute',
+              'Seituoda Taskforce Command',
+              'Spacelane Patrol',
+              'State and Region Bank',
+              'State Peacekeepers',
+              'State Protectorate',
+              'State War Academy',
+              'Storm Wind Strikeforce',
+              'Sukuuvestaa Corporation',
+              'Templis Dragonaurs',
+              'Top Down',
+              'Wiyrkomi Corporation',
+              'Wiyrkomi Peace Corps',
+              'Ytiri',
+              'Zainou',
+              'Zero-G Research Firm',
+              'Zumari Force Projection',
+              'CONCORD',
+              'DED',
+              'Inner Circle',
+              'Secure Commerce Commission',
+              'Algintal Core',
+              'Aliastra',
+              'Allotek Industries',
+              'Astral Mining Inc.',
+              'Bank of Luminaire',
+              'Center for Advanced Studies',
+              'Chatelain Rapid Response',
+              'Chemal Tech',
+              'Combined Harvest',
+              'Condotta Rouvenor',
+              'CreoDron',
+              'Crux Special Tasks Group',
+              'Duvolle Laboratories',
+              'Edimmu Warfighters',
+              'Egonics Inc.',
+              'Federal Administration',
+              'Federal Defense Union',
+              'Federal Freight',
+              'Federal Intelligence Office',
+              'Federal Marines',
+              'Federal Navy Academy',
+              'Federation Customs',
+              'Federation Navy',
+              'FedMart',
+              'Garoun Investment Bank',
+              'Impetus',
+              'Inner Zone Shipping',
+              'Kang Lo Directorate',
+              'Mannar Focused Warfare',
+              'Material Acquisition',
+              'Namtar Elite',
+              'Ostrakon Agency',
+              'Pend Insurance',
+              'Poteque Pharmaceuticals',
+              'President',
+              'Quafe Company',
+              'Resheph Interstellar Strategy',
+              'Roden Shipyards',
+              'Senate',
+              'Sinq Laison Gendarmes',
+              'Supreme Court',
+              'The Scope',
+              'TransStellar Shipping',
+              'University of Caille',
+              'Villore Sec Ops',
+              'Guristas',
+              'Guristas Production',
+              'Academy of Aggressive Behaviour',
+              'Genolution',
+              'Impro',
+              'Jove Navy',
+              'Jovian Directorate',
+              'Material Institute',
+              'Prosper',
+              'Shapeset',
+              'X-Sense',
+              'Khanid Innovation',
+              'Khanid Transport',
+              'Khanid Works',
+              'Royal Khanid Navy',
+              'Boundless Creation',
+              'Brutor Tribe',
+              'Brutor Vanguard',
+              'Circle of Huskarl',
+              'Core Complexion Inc.',
+              'Eifyr and Co.',
+              'Eyniletti Rangers',
+              'Forty-Nine Fedayeen',
+              'Freedom Extension',
+              'Krullefor Organization',
+              'Krusual Covert Operators',
+              'Krusual Tribe',
+              'Mikramurka Shock Troop',
+              'Minmatar Mining Corporation',
+              'Native Freshfood',
+              'Pator Tech School',
+              'Republic Command',
+              'Republic Fleet',
+              'Republic Justice Department',
+              'Republic Military School',
+              'Republic Parliament',
+              'Republic Security Services',
+              'Republic University',
+              'Sanmatar Kelkoons',
+              'Sebiestor Field Sappers',
+              'Sebiestor Tribe',
+              'Seykal Expeditionary Group',
+              'Six Kin Development',
+              'The Leisure Group',
+              'Tribal Liberation Force',
+              'Tronhadar Free Guard',
+              'Urban Management',
+              'Vherokior Combat Logistics',
+              'Vherokior Tribe',
+              'Outer Ring Excavations',
+              'True Creations',
+              'True Power',
+              'Serpentis Corporation',
+              'Serpentis Inquest',
+              'Food Relief',
+              'Sisters of EVE',
+              'The Sanctuary',
+              'InterBus',
+              'Society of Conscious Thought',
+              'Intaki Bank',
+              'Intaki Commerce',
+              'Intaki Space Police',
+              'Intaki Syndicate',
+              'Thukker Mix',
+              'Trust Partners',
+              'Arkombine']
+     
+
+     tup = ()
+     regionid = {'Derelik':10000001,'The Forge':10000002,'Vale of the Silent':10000003,'UUA-F4':10000004,'Detorid':10000005,'Wicked Creek':10000006,'Cache':10000007,'Scalding Pass':10000008,'Insmother':10000009,'Tribute':10000010,'Great Wildlands':10000011,'Curse':10000012,'Malpais':10000013,'Catch':10000014,'Venal':10000015,'Lonetrek':10000016,'J7HZ-F':10000017,'The Spire':10000018,'A821-A':10000019,'Tash-Murkon':10000020,'Outer Passage':10000021,'Stain':10000022,'Pure Blind':10000023,'Immensea':10000025,'Etherium Reach':10000027,'Molden Heath':10000028,'Geminate':10000029,'Heimatar':10000030,'Impass':10000031,'Sinq Laison':10000032,'The Citadel':10000033,'The Kalevala Expanse':10000034,'Deklein':10000035,'Devoid':10000036,'Everyshore':10000037,'The Bleak Lands':10000038,'Esoteria':10000039,'Oasa':10000040,'Syndicate':10000041,'Metropolis':10000042,'Domain':10000043,'Solitude':10000044,'Tenal':10000045,'Fade':10000046,'Providence':10000047,'Placid':10000048,'Khanid':10000049,'Querious':10000050,'Cloud Ring':10000051,'Kador':10000052,'Cobalt Edge':10000053,'Aridia':10000054,'Branch':10000055,'Feythabolis':10000056,'Outer Ring':10000057,'Fountain':10000058,'Paragon Soul':10000059,'Delve':10000060,'Tenerifis':10000061,'Omist':10000062,'Period Basis':10000063,'Essence':10000064,'Kor-Azor':10000065,'Perrigen Falls':10000066,'Genesis':10000067,'Verge Vendor':10000068,'Black Rise':10000069}
+     
+     for corporation in npc_stations:
+          url = 'http://evemaps.dotlan.net/npc/{0}/stations'.format(corporation)
+          parser = MyHTMLParser()
+          parser.feed(getstations(url))
+          moo = parser.returnvalue()
+          for y in moo.keys():
+               for z in moo[y]:
+                    tup += (y,)
+                    for q in z:
+                         tup += (q,)
+                    #print tup
+                    systems.append(tup)
+                    tup = ()     
+     #rows = []
+     conn = SQL.connect('eve_dump.db')
+     conn.text_factory = str
+     cur = conn.cursor()
+     #for x in regionid.keys():
+          #rows.append((x, regionid[x]))
+     
+     #print rows
+     cur.execute('''CREATE TABLE npcstation (Region, Constellation, System, Security, StationName, StationType)''')
+     conn.commit()
+     cur.executemany('INSERT INTO npcstation VALUES (?,?,?,?,?,?)',systems)
+     conn.commit() 
+     moop = cur.execute('SELECT * FROM npcstation')
+     print moop.fetchall()
+
+     
+     
+
+     #cur.execute('''CREATE TABLE regionid (RegionName, RegionId)''')
+     #conn.commit() 
+
+     
+          
+
+     #cur.executemany('INSERT INTO regionid VALUES (?,?)', rows)
+     #conn.commit()
+     conn.close()
+     
--- eve2.py
+++ eve2.py
@@ -1,7 +1,7 @@
 import requests as req
-import MySQLdb as SQL
 from xml.etree import ElementTree as ET
 from HTMLParser import HTMLParser
+import json as json
 
 regionid = {'Derelik':10000001,'The Forge':10000002,'Vale of the Silent':10000003,'UUA-F4':10000004,'Detorid':10000005,'Wicked Creek':10000006,'Cache':10000007,'Scalding Pass':10000008,'Insmother':10000009,'Tribute':10000010,'Great Wildlands':10000011,'Curse':10000012,'Malpais':10000013,'Catch':10000014,'Venal':10000015,'Lonetrek':10000016,'J7HZ-F':10000017,'The Spire':10000018,'A821-A':10000019,'Tash-Murkon':10000020,'Outer Passage':10000021,'Stain':10000022,'Pure Blind':10000023,'Immensea':10000025,'Etherium Reach':10000027,'Molden Heath':10000028,'Geminate':10000029,'Heimatar':10000030,'Impass':10000031,'Sinq Laison':10000032,'The Citadel':10000033,'The Kalevala Expanse':10000034,'Deklein':10000035,'Devoid':10000036,'Everyshore':10000037,'The Bleak Lands':10000038,'Esoteria':10000039,'Oasa':10000040,'Syndicate':10000041,'Metropolis':10000042,'Domain':10000043,'Solitude':10000044,'Tenal':10000045,'Fade':10000046,'Providence':10000047,'Placid':10000048,'Khanid':10000049,'Querious':10000050,'Cloud Ring':10000051,'Kador':10000052,'Cobalt Edge':10000053,'Aridia':10000054,'Branch':10000055,'Feythabolis':10000056,'Outer Ring':10000057,'Fountain':10000058,'Paragon Soul':10000059,'Delve':10000060,'Tenerifis':10000061,'Omist':10000062,'Period Basis':10000063,'Essence':10000064,'Kor-Azor':10000065,'Perrigen Falls':10000066,'Genesis':10000067,'Verge Vendor':10000068,'Black Rise':10000069}
 
@@ -9,17 +9,27 @@ regionid = {'Derelik':10000001,'The Forge':10000002,'Vale of the Silent':1000000
 
 
 
-
-def route(start, finish):
+def numberjumpsroute(start, finish):
      path = 'http://api.eve-central.com/api/route/from/{0}/to/{1}'.format(start,finish)
      rv = req.get(path)
-     return rv.text
+     return len(rv.json())
 
 def getstations(url):
      rv = req.get(url)
      return rv.text
 
-
+def evapi(arg):
+     from functools import wraps
+     charID = 0
+     @wraps(arg)
+     def wrapper(*args):
+          xml = ET.fromstring(arg(*args))
+          for node in xml.iter():
+               if node.tag == 'row':
+                    charID = node.attrib['characterID']
+          return charID
+     return wrapper
+     
      
 
 def quicklook(arg):
@@ -86,7 +96,13 @@ def marketstat(arg):
 def profit(buyvol, sellmin, buymax):
      
      return (float(buyvol) * (float(buymax)-float(sellmin)))
-     
+
+@evapi
+def eveid(objectname):
+     path = 'https://api.eveonline.com/eve/CharacterID.xml.aspx'
+     payload = {'names':objectname}
+     rv = req.get(path, params=payload)
+     return rv.text    
 
 @marketstat
 def querymarketstat(name):
@@ -130,92 +146,41 @@ def hiseconly(orders, security):
 def maxprice(orders):
      '''returns the highest price for orders with a significant volume remaining'''
      max_price = 0
+     max_order = {}
+
      for x in orders.keys():
           if float(orders[x]['price']) < max_price:
-               del orders[x]
+               #del orders[x]
+               pass
           else:
-               max_price = orders[x]['price']          
-               
-     return orders
+               max_order = orders[x]
+               max_price = orders[x]['price']
+          
+     return max_order
 
 def minprice(orders):
      '''returns the highest price for orders with a significant volume remaining'''
      min_price = 9999999999
+     min_order = {}
+
      for x in orders.keys():
           if float(orders[x]['price']) > min_price:
-               del orders[x]
+               #del orders[x]
+               pass
           else:
+               min_order = orders[x]
                min_price = orders[x]['price']
                
-     return orders
+     return min_order
 
-class MyHTMLParser(HTMLParser):
-     def __init__(self):
-          HTMLParser.__init__(self)
-          self.intable = False
-          self.value = {}
-          self.diction = []
-          self.righttable = False
-          self.a = False
-          self.h2 = False
-          self.regionname = False
-          self.region = ""
-          self.working = []
-          
-     def handle_starttag(self, tag, attrs):
-          if tag == 'table':
-               self.intable = True
-          elif tag == 'h2':
-               self.h2 = True
-          elif tag == 'a':
-               self.a = True
-               for x in attrs:
-                    #print x
-                    if '/region/' in x[1].encode('ascii', 'ignore'):
-                         self.regionname = True
-                                
-     def handle_endtag(self, tag):
-          if tag == 'table':
-               self.intable = False
-               if self.righttable == True:
-                    self.value[self.region] = self.working
-                    self.working = []
-                    self.righttable = False
-          elif tag  == 'tr' and self.righttable == True:
-                              
-               self.working.append(self.diction)
-               #print self.region
-               
-               self.diction = []
-          
-          elif tag == 'h2':
-               self.h2 = False
-               self.regionname = False
-          elif tag == 'a':
-               self.regionname = False
-               self.a == False
-               
-               
-     def handle_data(self, data):
-          if self.h2 == True and self.a == True and self.regionname == True:
-               temp = data.encode('ascii', 'ignore')
-               #print temp
-               self.region = temp
-          if 'Constellation' in data and self.intable == True:
-               self.righttable = True
-          if self.righttable == True:
-               temp = data.encode('ascii', 'ignore')
-               if not '\n' in temp and temp != ' ':
-                    self.diction.append(temp)
-     
-     def returnvalue(self):
-          return self.value
+
                     
      
 
 if __name__=="__main__":
      
-     
+     sell = []
+     buy = []
      #data = querymarketstat('Tritanium')
      #tritanium_heimatar = queryquicklook('Tritanium', 'Heimatar')
      #tritanium_SinqLaison = queryquicklook('Tritanium', 'Sinq Laison')
@@ -227,74 +192,51 @@ if __name__=="__main__":
      #print(maximum(tritanium_SinqLaison[1],1000), 'maxi buy')
      #sell_tritanium_TheForge_hisec = hiseconly(tritanium_TheForge[0],0.5)
      #buy_tritanium_TheForge_hisec = hiseconly(tritanium_TheForge[1],0.5)
-     #print route(10000056, 10000043)
+     #print route(10000022, 10000033)
      
      #max_price = maxprice(buy_tritanium_TheForge_hisec)
-     #for x in regionid.keys():
-          #xorders = queryquicklook('Pyerite', x, 20000)
-          #sell_xorders_tritanium_highsec = hiseconly(xorders[0],0.5)
-          #buy_xorders_tritanium_highsec = hiseconly(xorders[1],0.5)
-          #print (x, minprice(sell_xorders_tritanium_highsec), maxprice(buy_xorders_tritanium_highsec))
-          
-          #minsell = minprice(sell_xorders_tritanium_highsec, 1000)
-          #minsell = 2.00
+     print eveid('Tritanium')
+     buyercount = 0
+     salecount = 9999999999
+     '''for x in regionid.keys():
+          xorders = queryquicklook('Tritanium', x, 20000)
+          sell_xorders_tritanium_highsec = hiseconly(xorders[0],0.5)
+          buy_xorders_tritanium_highsec = hiseconly(xorders[1],0.5)
+          sale = minprice(sell_xorders_tritanium_highsec)
+          buyer = maxprice(buy_xorders_tritanium_highsec)
+          if sale !={} and float(sale['price']) < salecount:
+               salecount = float(sale['price'])
+               sell = sale
+
+          if buyer != {} and float(buyer['price']) > buyercount:
+               buyercount = float(buyer['price'])
+               buy = buyer
+               
+
+     #print sell['price'], sell['station_name']
+     #print buy['price'], buy['station_name']
+
      
-     amarr1 = ['24th Imperial Crusade', 
-              'Amarr Certified News', 
-              'Amarr Civil Service', 
-              'Amarr Constructions', 
-              'Amarr Navy', 
-              'Amarr Templars', 
-              'Amarr Trade Registry', 
-              'Ametat Security', 
-              'Ardishapur Family', 
-              'Bragian Order', 
-              'Carthum Conglomerate', 
-              'Civic Court', 
-              'Company of Marcher Lords', 
-              'Court Chamberlain', 
-              'Ducia Foundry', 
-              'Emperor Family', 
-              'Fraternity of St. Venefice', 
-              'Further Foodstuffs', 
-              'Hedion University', 
-              'Holdfast Syndicate', 
-              'HZO Refinery', 
-              'Imperial Academy', 
-              'Imperial Armaments', 
-              'Imperial Chancellor', 
-              'Imperial Guard', 
-              'Imperial Shipment', 
-              'Inherent Implants', 
-              'Joint Harvesting', 
-              'Kador Family', 
-              'Kameira Lodge', 
-              'Kor-Azor Family', 
-              'Ministry of Assessment', 
-              'Ministry of Internal Order', 
-              'Ministry of War', 
-              'Noble Appliances', 
-              'Nurtura', 
-              'Paladin Survey Force', 
-              'Red and Silver Hand', 
-              'Royal Amarr Institute', 
-              'Royal Uhlans', 
-              'Sarum Family', 
-              'Shining Flame', 
-              'Tal-Romon Legion', 
-              'Tash-Murkon Family', 
-              'Theology Council', 
-              'Viziam', 
-              'Zoar and Sons']
-     amarr = ['Zoar and Sons']
+     #for reg0 in sell:
+          #for reg1 in buy:
+               #profit = (float(reg1['price']) - float(reg0['price']) )* float(reg0['vol_remain'])
+               #numjump = 
+               #f profit >= oldprofit:
+                    
      
-     for x in amarr:
-          url = 'http://evemaps.dotlan.net/npc/{0}/stations'.format(x)
-          parser = MyHTMLParser()
-          parser.feed(getstations(url))
-          moo = parser.returnvalue()
-          print moo
-          #for i in moo:
-               #print  i[0],i[1],i[3]
+     import sqlite3 as SQL
+     conn = SQL.connect('eve_dump.db')
+     conn.text_factory = str
+     cur = conn.cursor()
+     cur.execute('SELECT System FROM npcstation WHERE StationName=?', (sell['station_name'],))
+     sqlthing1 = cur.fetchone()
+     cur.execute('SELECT System FROM npcstation WHERE StationName=?', (buy['station_name'],))
+     sqlthing2 = cur.fetchone()
+
+     print numberjumpsroute(sqlthing1[0], sqlthing2[0]) , (float(buy['price']) - float(sell['price']))
+
+     conn.close()'''
+
+
 
      
